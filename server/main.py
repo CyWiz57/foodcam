b8e9a09846e49b2748270e8c8d80aff033f6c8db
firoz <mr.firozkhan57@gmail.com>
2021-07-16 22:20:41 +0530

    back to normal

--- server/main.py
+++ server/main.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
 from flask import render_template
-import torchvision.transforms as T
-from PIL import Image
 from fastai.vision.all import *
 
 
@@ -20,11 +18,8 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    # img = PILImage.create(request.files['file'])
-    img_pil = Image.open('file')
-    img_tensor = T.ToTensor()(img_pil)
-    img_fastai = Image(img_tensor)
-    label,_,probs = learn.predict(img_fastai)
+    img = PILImage.create(request.files['file'])
+    label,_,probs = learn.predict(img)
     return f'{label} ({torch.max(probs).item()*100:.0f}%)'
 
 if __name__=='__main__':
